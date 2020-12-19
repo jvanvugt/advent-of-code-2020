cb1b01656f69cf81f812a77be0de67045bf8f204
Joris van Vugt <jorisvanvugt@gmail.com>
2020-12-19 11:53:47 +0100

    Finish day 19

--- day19.cpp
+++ day19.cpp
@@ -7,61 +7,46 @@ using namespace std;
 using SubRule = vector<string>;
 using Rule = vector<SubRule>;
 
+vector<string_view> matches_rule(const string_view& message, const vector<Rule>& rules, const Rule& rule);
 
-optional<string_view> matches_rule(const string_view& message, const vector<Rule>& rules, size_t rule_idx);
-
-optional<string_view> match_subrule(string_view message, const vector<Rule>& rules, const SubRule& subrule)
+vector<string_view> match_subrule(string_view message, const vector<Rule>& rules, const SubRule& subrule, size_t si = 0)
 {
-    for (const auto& part : subrule)
+    if (si == subrule.size())  // Finished matching this subrule
+        return {message};
+    const auto& part = subrule[si];
+    if (!isdigit(part.at(0)))  // Matching a char literal
     {
-        if (isdigit(part.at(0)))
-        {
-            size_t next_rule_idx = stoull(part);
-            auto maybe_match = matches_rule(message, rules, next_rule_idx);
-            if (!maybe_match)
-                return {};
-            message = *maybe_match;
-        }
+        if (message.length() > 0 && message.at(0) == part[0])
+            return match_subrule(message.substr(1), rules, subrule, si + 1);
         else
-        {
-            assert(part == "a" || part == "b");
-            char c = part[0];
-            if (message.length() > 0 && message.at(0) == c)
-                message.remove_prefix(1);
-            else
-                return {};
-        }
+            return {};
     }
-    return message;
+
+    vector<string_view> possibilities;
+    for (const auto& match : matches_rule(message, rules, rules[stoull(part)]))
+    {
+        auto rest_matches = match_subrule(match, rules, subrule, si + 1);
+        possibilities.insert(possibilities.end(), rest_matches.begin(), rest_matches.end());
+    }
+    return possibilities;
 }
 
-
-optional<string_view> matches_rule(const string_view& message, const vector<Rule>& rules, size_t rule_idx = 0)
+vector<string_view> matches_rule(const string_view& message, const vector<Rule>& rules, const Rule& rule)
 {
-    const Rule& rule = rules.at(rule_idx);
     vector<string_view> possibilities;
     for (const auto& subrule : rule)
     {
-        auto maybe_match = match_subrule(message, rules, subrule);
-        if (maybe_match)
-            possibilities.push_back(*maybe_match);
+        auto submatches = match_subrule(message, rules, subrule);
+        possibilities.insert(possibilities.end(), submatches.begin(), submatches.end());
     }
-    if (possibilities.size() > 1)
-    {
-        return *ranges::min_element(possibilities, {}, [](const auto& sv) { return sv.length(); });
-    }
-    if (possibilities.size() == 1)
-    {
-        return possibilities[0];
-    }
-    return {};
+    return possibilities;
 }
 
 int a(const vector<Rule>& rules, const vector<string>& messages)
 {
     return ranges::accumulate(messages, 0, [&rules](int acc, const string& msg) {
-        auto maybe_match = matches_rule(msg, rules);
-        return acc + (maybe_match ? (*maybe_match).length() == 0 : false);
+        auto matches = matches_rule(msg, rules, rules[0]);
+        return acc + ranges::any_of(matches, [](const auto& m) { return m.length() == 0; });
     });
 }
 
@@ -70,8 +55,8 @@ int b(vector<Rule> rules, const vector<string>& messages)
     rules[8] = {{"42"}, {"42", "8"}};
     rules[11] = {{"42", "31"}, {"42", "11", "31"}};
     return ranges::accumulate(messages, 0, [&rules](int acc, const string& msg) {
-        auto maybe_match = matches_rule(msg, rules);
-        return acc + (maybe_match ? (*maybe_match).length() == 0 : false);
+        auto matches = matches_rule(msg, rules, rules[0]);
+        return acc + ranges::any_of(matches, [](const auto& m) { return m.length() == 0; });
     });
 }
 
@@ -86,17 +71,13 @@ int main()
     {
         auto num_and_rule = aoc::split(rule_line, ": ");
         size_t idx = stoull(num_and_rule.at(0));
-        Rule rule;
         for (const auto& subrule_s : aoc::split(num_and_rule.at(1), " | "))
         {
             SubRule subrule;
             for (const auto& part : aoc::split(subrule_s, " "))
-            {
                 subrule.push_back(part.at(0) == '"' ? part.substr(1, part.length() - 2) : part);
-            }
-            rule.push_back(subrule);
+            rules[idx].push_back(subrule);
         }
-        rules[idx] = rule;
     }
     cout << a(rules, messages) << endl;
     cout << b(rules, messages) << endl;
